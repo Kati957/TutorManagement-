8d2f4cdec62a652f41f85d82511f42919055e39b
Hungnv <shin.map957@gmail.com>
2025-03-01 22:50:43 +0700

    update lib

--- src/java/model/DBConnect.java
+++ src/java/model/DBConnect.java
@@ -32,7 +32,7 @@ public class DBConnect {
         } 
     }
     public DBConnect(){
-        this("jdbc:sqlserver://localhost:1433;databaseName=test1", "sa","123");
+        this("jdbc:sqlserver://localhost:1433;databaseName=G4_TutorManagement", "sa","123456");
     }
     
     public static void main(String[] args){
